faab37d8891cb3b7663bc6e73eac1ef199ec0a63
khanh.ngoc <khanh.ngoc@coccoc.com>
2019-08-05 20:54:27 +0700

    Add some to server

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const bodyParser = require('body-parser');
 
 // Create a new instance of express
@@ -9,10 +10,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post('/question', function (req, res) {
-  const body = req.body;
-  console.log(req.body);
-  res.set('Content-Type', 'text/plain');
-  res.send(`You sent: ${body} to Express`);
+  // console.log(req.body.question);
+  // const question = req.body.question;
+  // const answer1 = req.body.answer1;
+  // const answer2 = req.body.answer2;
+  // const answer3 = req.body.answer3;
+  // let tokenizedQuestion = tokenzieQuestion(question);
+  // makeQuery(tokenizedQuestion, answer1, answer2, answer3).then((result) => {
+  //   res.send(result);
+  // });
+  var options = {
+    url: `http://www.google.com/search?q=${encodeURI('kẹo+dừa+là+đặc+sản+tỉnh')}`,
+    headers: {
+      'User-Agent': 'Mozilla/5.0'
+    },
+    encoding: 'utf8'
+  };
+  request(options, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    res.end(body);
+  });
 })
 
 // Tell our app to listen on port 3000
@@ -23,3 +42,15 @@ app.listen(port, function (err) {
 
   console.log(`Server started on port ${port}`);
 })
+
+function tokenzieQuestion(question) {
+
+}
+
+function makeQuery(question, answer1, answer2, answer3) {
+
+}
+
+function parseHTML() {
+
+}
